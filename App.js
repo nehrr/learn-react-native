9012d12e17ab09ad37bd3dcda659f7867d2d8403
nehrr <iamkapitaan@gmail.com>
2018-04-13 14:57:08 +0200

    challenge 2 done

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default class App extends React.Component {
   render() {
+    console.disableYellowBox = true;
     return (
       <View style={styles.container}>
-        <Text>I love CodingBad</Text>
+        <Text style={styles.texts}>Something</Text>
+        <Image style={styles.avatar} source={require("./fenris.png")} />
+        <View style={styles.texts}>
+          <Text style={styles.nested}>
+            *
+            <Text style={styles.textLeft}>left</Text>
+            *
+          </Text>
+          <Text style={styles.nested}>
+            *
+            <Text style={styles.testRight}>right</Text>
+            *
+          </Text>
+        </View>
       </View>
     );
   }
@@ -17,5 +31,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+
+  avatar: {
+    borderRadius: 75,
+    borderWidth: 1,
+    borderColor: "#000000",
+    width: 150,
+    height: 150,
+    marginTop: 20
+  },
+
+  textLeft: {
+    color: "#4286f4",
+    textAlign: "left"
+  },
+
+  testRight: {
+    color: "#f44141",
+    textAlign: "right"
+  },
+
+  texts: {
+    flexDirection: "row"
+  },
+
+  nested: {
+    padding: 20
   }
 });
