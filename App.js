d7f07e354bfba18653eaccfd88482bafa7f6eaad
nehrr <iamkapitaan@gmail.com>
2018-04-16 17:55:22 +0200

    list showing

--- App.js
+++ App.js
@@ -4,108 +4,76 @@ import {
   Text,
   View,
   Image,
-  Switch,
-  Button,
-  Dimensions,
-  TouchableOpacity
+  ScrollView,
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
+import { StackNavigator } from "react-navigation";
+import _ from "lodash";
+import pokemons from "./pokemon.json";
+
+const { width, height } = Dimensions.get("window");
+
+class DetailView extends React.Component {}
+
+class Pokedex extends React.Component {
+  static navigationOptions = {
+    title: "Pokedex"
+  };
+
+  render() {
+    // console.log(this.state.pokemons);
+
+    const list = pokemons.map(pokemon => {
+      return (
+        <TouchableOpacity style={styles.cell}>
+          <View key={pokemon.id} style={styles.item}>
+            <Text style={styles.text}>{_.capitalize(pokemon.name)}</Text>
+          </View>
+        </TouchableOpacity>
+      );
+    });
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ScrollView>{list}</ScrollView>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isButtonEnabled: false,
-      label: "Disabled",
-      isGifDisplayed: false
-    };
-  }
-
   render() {
     console.disableYellowBox = true;
 
-    return (
-      <View style={styles.container}>
-        {this.state.isGifDisplayed ? this.ViewGif() : this.ViewBase()}
-      </View>
-    );
+    return <Root />;
   }
-
-  ViewBase() {
-    return (
-      <View style={styles.container}>
-        <Text style={[styles.nested, styles.text]}>{this.state.label}</Text>
-        <Switch
-          style={styles.nested}
-          value={this.state.isButtonEnabled}
-          onValueChange={this.changeValue}
-        />
-        <View style={styles.whiteButton}>
-          <Button
-            title="Let's Rock"
-            style={styles.nested}
-            disabled={!this.state.isButtonEnabled}
-            onPress={this.buttonPressed}
-          />
-        </View>
-      </View>
-    );
-  }
-
-  ViewGif() {
-    //for use with multiple views to take whole screen
-    const { width, height } = Dimensions.get("window");
-
-    return (
-      <TouchableOpacity
-        onPress={() => this.setState({ isGifDisplayed: false })}
-      >
-        <View style={(width, height)}>
-          <Image style={styles.gif} source={require("./reaper.gif")} />
-        </View>
-      </TouchableOpacity>
-    );
-  }
-  changeValue = value => {
-    let label = this.state.label === "Enabled" ? "Disabled" : "Enabled";
-    this.setState({ isButtonEnabled: value, label });
-    console.log(value);
-  };
-
-  //To keep the 'this'
-  buttonPressed = () => {
-    this.setState({ isGifDisplayed: true });
-    console.log("button pushed");
-  };
 }
+
+const Root = StackNavigator({
+  Pokedex: {
+    screen: Pokedex
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#DEDEDE",
+    backgroundColor: "#FFFFFF",
     alignItems: "center",
     justifyContent: "center"
   },
 
-  whiteButton: {
-    backgroundColor: "#fff",
-    margin: 20
-  },
-
-  imageContainer: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-
-  gif: {
-    flex: 1,
-    resizeMode: "stretch"
-  },
-
   text: {
     fontSize: 20,
+    padding: 10,
+    textAlign: "center"
+  },
+
+  cell: {
+    borderWidth: 1,
+    borderColor: "#E5443B",
+    width: width,
     padding: 20
   },
 
